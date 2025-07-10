f62b6ff5434b525f2170e2fac1d4170434062066
Kim_Wongyum <rnr0417@naver.com>
2025-07-10 11:50:25 +0900

    Complete

--- boilerplate1/lib/main.dart
+++ boilerplate1/lib/main.dart
@@ -2,36 +2,111 @@ import 'package:flutter/material.dart';
 
 /// Flutter code sample for [Scaffold].
 
-void main() => runApp(const ScaffoldExampleApp());
+void main() => runApp(const MyApp());
 
-class ScaffoldExampleApp extends StatelessWidget {
-  const ScaffoldExampleApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ScaffoldExample());
+    return const MaterialApp(
+      home: Boilerplate(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class ScaffoldExample extends StatefulWidget {
-  const ScaffoldExample({super.key});
+class Boilerplate extends StatefulWidget {
+  const Boilerplate({super.key});
 
   @override
-  State<ScaffoldExample> createState() => _ScaffoldExampleState();
+  State<Boilerplate> createState() => _BoilerplateState();
 }
 
-class _ScaffoldExampleState extends State<ScaffoldExample> {
-  int _count = 0;
+class _BoilerplateState extends State<Boilerplate> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sample Code')),
-      body: Center(child: Text('You have pressed the button $_count times.')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _count++),
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
+      appBar: AppBar(
+        backgroundColor: Color(0xff4B6EB1),
+        title: const Text(
+          'SODA',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: false,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu, color: Colors.white),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        surfaceTintColor: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Color(0xff182949)),
+              child: Text(
+                'SODA',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite, color: Color(0xff7B7A7A)),
+              title: const Text(
+                'Icon : favorite',
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(child: Text('Copyright 2022 SODA  All rights reserved.')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.error),
+            label: 'Information',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+        ],
+
+        currentIndex: _selectedIndex,
+        showUnselectedLabels: true,
+        unselectedItemColor: Color(0xff979797),
+        selectedItemColor: Color(0xff4B6EB1),
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
