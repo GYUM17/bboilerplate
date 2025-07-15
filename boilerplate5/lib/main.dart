9756e326f2612fff3ce926b776642e4575ae7cda
Kim_Wongyum <rnr0417@naver.com>
2025-07-15 13:56:27 +0900

    Delete test codes

--- boilerplate5/lib/main.dart
+++ boilerplate5/lib/main.dart
@@ -9,6 +9,7 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(backgroundColor: Colors.white, body: MyPageView()),
     );
   }
